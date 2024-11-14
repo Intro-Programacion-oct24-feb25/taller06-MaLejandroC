da3fdcffbf70f0ffc7201de67b71d9676204eb44
MalejandroC <macarrion44@utpl.edu.ec>
2024-11-14 17:42:13 -0500

    Problema02

--- problema02/Problema002/src/problema002/Problema002.java
+++ problema02/Problema002/src/problema002/Problema002.java
@@ -2,70 +2,69 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package problema2;
+package problema002;
 
 import java.util.Scanner;
 import java.util.Locale;
+
 /**
  *
  * @author utpl
  */
-public class Problema2 {
+public class Problema002 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here\
-        
+
         Scanner valores = new Scanner(System.in);
         valores.useLocale(Locale.US);
-        
+
         String propietario;
         double valorVh;
         int tipo;
         String ntipo = "";
         double calculo = 0;
-        
-        
+
         System.out.print("Ingrese el nombre propietario: ");
         propietario = valores.nextLine();
         System.out.print("Ingrese el valor del vehiculo: ");
         valorVh = valores.nextDouble();
         System.out.print("Ingrese el tipo de vehiculo: ");
         tipo = valores.nextInt();
-        
+
         switch (tipo) {
-            case 1: 
+            case 1:
                 ntipo = "vehículo liviano particular";
-                calculo = (valorVh*1)/100 + 2;
+                calculo = (valorVh * 0.01) / 100 + 2;
                 break;
-                
-            case 2: 
+
+            case 2:
                 ntipo = "vehículo grande particular";
-                calculo = (valorVh*2)/100 + 2.5;
+                calculo = (valorVh * 0.02) / 100 + 2.5;
                 break;
-                
-                case 3: 
+
+            case 3:
                 ntipo = "taxi";
-                calculo = (valorVh*4)/100 + 1.5;
+                calculo = (valorVh * 0.04) / 100 + 1.5;
                 break;
-                
-                case 4: 
+
+            case 4:
                 ntipo = "bus urbano";
-                calculo = (valorVh*5)/100 + 2.2;
+                calculo = (valorVh * 0.05) / 100 + 2.2;
                 break;
 
         }
-        
-        System.out.printf("Peaje "+"buena via\n\tPropietario:%s\nTipo:%s\n\tvalor: $%.0f\n\tPeaje: $%.2f",
+
+        System.out.printf("Peaje \"buena via\"\n\tPropietario:%s\nTipo: %s\n\t"
+                + "valor: $%.0f\n\tPeaje: $%.1f",
                 propietario,
                 ntipo,
                 valorVh,
                 calculo);
-        
-        
-        
+
     }
-    
+
 }
